dee46e60cf96ad8daaa5b7a1d0397a075f188925
TrickyPells <xavier.peller1@gmail.com>
2019-05-28 00:08:39 +0200

    Ajout d'une DocString a Interfaire.py

--- InterFaire.py
+++ InterFaire.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
 #  Copyright (c) 2019. Writing by TrickyPells.
+"""Ce programme a pour but de présenter une liste de tâches. L'utilisateur peut en choisir une, le script
+lance alors les programmes nécessaires pour cette tâche.
 
+"""
+
+import os
+import time
+import tkinter
+
+#  Le script...
